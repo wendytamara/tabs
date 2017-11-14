f786c5539cb1b48d81e060d76271824a274a75c8
wendytamara <anacarlavegam@gmail.com>
2017-11-13 23:14:34 -0500

    avance j

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
+var mostrarOcultar = function(e) {
 
+    var tabSeleccionado = e.target.dataset.tabSeleccionado;
+    if (tabSeleccionado == "desayuno") {
+        console.log("vamos a desayunar");
+    } else if (tabSeleccionado == "almuerzo") {
+       console.log("vamos a almorzar");
+    } else if (tabSeleccionado== "cena")  {
+          console.log("vamos a cenar");
+    }
+};
+
+var cargarPagina = function() {
+
+    var elementosTabs = document.getElementsByClassName("tabs");
+    for (var i = 0; i < elementosTabs.length; i++) {
+      elementosTabs[i].addEventListener("click", mostrarOcultar);
+    }
+};
+
+cargarPagina();
